c5038a2467e24cd40bd2db169ed8c913cbca778b
lazycoder <syedashrafullah15@gmail.com>
2018-05-28 23:46:26 +0600

    #initialize app

---
+++ app/src/main/java/net/a6te/lazycoder/weatherapp/SplashScreen.java
@@ -0,0 +1,13 @@
+package net.a6te.lazycoder.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
